2d736ff9a0fd861b37eae44e6fa20a25244c94f7
Jordi Ruiz <jordiruizroman21@gmail.com>
2025-10-09 21:23:16 +0200

    Primer commit portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+ const text = "Soy un desarrollador web junior apasionado por crear experiencias web modernas y funcionales. He trabajado con tecnologías como HTML, CSS, JavaScript, React y Django. Actualmente estoy buscando una oportunidad para aplicar mis conocimientos y seguir aprendiendo.";
+  let i = 0;
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("typing").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 20);
+    }
+  }
+  typeWriter();
+
+
+  
