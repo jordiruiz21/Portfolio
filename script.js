3f691c1e0a8433ae0ea9a6210293dbf3bcfda36a
Jordi Ruiz <jordiruizroman21@gmail.com>
2025-11-29 20:40:49 +0100

    Proyecto 4 añadido

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
- // 1. EFECTO DE ESCRITURA (Typewriter)
+ 
+ //Menú desplegable
+ const btn = document.getElementById('mobile-menu-btn');
+    const menu = document.getElementById('mobile-menu');
+    const icon = btn.querySelector('i');
+    const mobileLinks = document.querySelectorAll('.mobile-link');
+
+    // 1. Abrir/Cerrar menú al pulsar el botón
+    btn.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+      
+      // Cambiar icono de Hamburguesa (fa-bars) a X (fa-times)
+      if (menu.classList.contains('hidden')) {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+      } else {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+      }
+    });
+
+    // 2. Cerrar menú automáticamente al hacer click en un enlace
+    mobileLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        menu.classList.add('hidden');
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+      });
+    });
+ 
+ 
+ // EFECTO DE ESCRITURA (Typewriter)
     const textToType = "Soy un desarrollador web junior apasionado por crear experiencias web modernas y funcionales. Me he graduado en Desarrollo de Aplicaciones Web y en Diseño y Edición de Publicaciones Impresas y Multimedia.\n He trabajado con tecnologías como HTML, CSS, JavaScript, React y Django, entre otras. Actualmente estoy buscando una oportunidad para aplicar mis conocimientos y seguir aprendiendo.";
     const typingElement = document.getElementById('typing-container');
     let i = 0;
@@ -26,7 +57,7 @@
     observer.observe(document.getElementById('sobremi'));
 
 
-    // 2. EFECTO SPOTLIGHT EN LAS CARDS
+    // EFECTO SPOTLIGHT EN LAS CARDS
     // Este script hace que el borde brillante siga al ratón dentro del contenedor
     const cardsContainer = document.getElementById("cards-container");
     const cards = document.querySelectorAll(".card");
